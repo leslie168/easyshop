89a258bcc22260fea74ae5cbbd0b980c068f4165
leslie <leslie@lesliedembp>
2016-09-11 10:48:40 +0800

    create new

---
+++ easyshop-order/src/main/java/com/easyshop/order/clientlib/customer/CustomerClient.java
@@ -0,0 +1,25 @@
+package com.easyshop.order.clientlib.customer;
+
+
+import com.netflix.hystrix.HystrixObservable;
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import rx.Observable;
+
+import java.util.List;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@FeignClient("customer-service")
+public interface CustomerClient {
+
+
+
+	 @RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	 CustomerView getCustomerViewByUserId(@PathVariable("userId") String userId) ;
+	    
+
+}
