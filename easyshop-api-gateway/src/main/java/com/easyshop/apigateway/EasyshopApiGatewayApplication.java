d355bb0c045ce613b5721ba9fdf73158b1da5afd
leslie <leslie@lesliedembp>
2016-09-28 21:04:20 +0800

    update auth server

--- easyshop-api-gateway/src/main/java/com/easyshop/apigateway/EasyshopApiGatewayApplication.java
+++ easyshop-api-gateway/src/main/java/com/easyshop/apigateway/EasyshopApiGatewayApplication.java
@@ -2,12 +2,15 @@ package com.easyshop.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@EnableOAuth2Sso
 public class EasyshopApiGatewayApplication {
 
 	public static void main(String[] args) {
